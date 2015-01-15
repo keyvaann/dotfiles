71b4844df1814001338bf94cf4ec6b58469b39c1
Keyvan Hedayati <k1.hedayati93@gmail.com>
2015-01-15 16:26:38 +0330

    Added new staff to emacs config

--- config.el
+++ config.el
@@ -185,7 +185,6 @@ might be bad.
 (global-set-key (kbd "C-k") 'kill-and-join-forward)
 (global-set-key (kbd "C-<tab>") 'other-window)
 (global-set-key (kbd "C-x C-c") 'delete-frame) ;; the mnemonic is C-x REALLY QUIT
-(global-set-key (kbd "C-x <f1>") 'ispell)
 (global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal) ;; I don't need to kill emacs that easily
 (global-set-key (kbd "C-x t c") 'highlight-changes-mode)
 (global-set-key (kbd "M-;") 'comment-dwim-line)
@@ -193,11 +192,6 @@ might be bad.
 ;; http://whattheemacsd.com/key-bindings.el-03.html
 (global-set-key (kbd "M-j") (lambda () (interactive) (join-line -1))) ; joins the following line onto this one.
 (global-set-key (kbd "C-z") 'repeat)
-;; Transpose stuff with M-t
-(global-unset-key (kbd "M-t")) ;; which used to be transpose-words
-(global-set-key (kbd "M-t l") 'transpose-lines)
-(global-set-key (kbd "M-t w") 'transpose-words)
-(global-set-key (kbd "M-t s") 'transpose-sexps)
 ;; Defining some useful keybindings
 (global-set-key (kbd "C-c l") 'mark-line)
 ;;
@@ -205,76 +199,15 @@ might be bad.
 (global-set-key "\M-0" 'forward-sexp)
 (global-set-key "\M-1" 'delete-other-windows)
 
-(define-key ido-file-dir-completion-map
-  [remap set-mark-command]  'ido-restrict-to-matches)
-
 (message "5. Key bindings successfully defined.")
 
 ;; always need a scrach
 (run-with-idle-timer 1 t
                      '(lambda () (get-buffer-create "*scratch*")))
 
-(add-to-list 'clean-buffer-list-kill-regexps
-             '("\\`\\*Customize .*\\*\\'"
-               "\\`\\*\\(Wo\\)?Man .*\\*\\'"))
-
-;; Hyphen on Space
-;; modify smex so that typing a space will insert a hyphen ‘-’ like in normal M-x
-;; http://www.emacswiki.org/emacs/Smex
-(defadvice smex (around space-inserts-hyphen activate compile)
-        (let ((ido-cannot-complete-command
-               `(lambda ()
-                  (interactive)
-                  (if (string= " " (this-command-keys))
-                      (insert ?-)
-                    (funcall ,ido-cannot-complete-command)))))
-          ad-do-it))
-
-;;; Filters ido-matches setting acronynm matches in front of the results
-;; http://www.emacswiki.org/emacs/Smex
-(defadvice ido-set-matches-1 (after ido-acronym-matches activate)
-  (if (> (length ido-text) 1)
-      (let ((regex (concat "^" (mapconcat 'char-to-string ido-text "[^-]*-")))
-            (acronym-matches (list))
-            (remove-regexes '("-menu-")))
-        ;; Creating the list of the results to be set as first
-        (dolist (item items)
-          (if (string-match (concat regex "[^-]*$") item) ;; strict match
-              (add-to-list 'acronym-matches item)
-            (if (string-match regex item) ;; appending relaxed match
-                (add-to-list 'acronym-matches item t))))
-
-        ;; Filtering ad-return-value
-        (dolist (to_remove remove-regexes)
-          (setq ad-return-value
-                (delete-if (lambda (item)
-                             (string-match to_remove item))
-                           ad-return-value)))
-
-        ;; Creating resulting list
-        (setq ad-return-value
-              (append acronym-matches
-                      ad-return-value))
-
-        (delete-dups ad-return-value)
-        (reverse ad-return-value))))
-
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
   "Prevent annoying \"Active processes exist\" query when you quit Emacs."
   (cl-flet ((process-list ())) ad-do-it))
-
-;; Keep region when undoing in region
-;; http://whattheemacsd.com/my-misc.el-02.html
-(defadvice undo-tree-undo (around keep-region activate)
-  (if (use-region-p)
-      (let ((m (set-marker (make-marker) (mark)))
-            (p (set-marker (make-marker) (point))))
-        ad-do-it
-        (goto-char p)
-        (set-mark m)
-        (set-marker p nil)
-        (set-marker m nil))
-    ad-do-it))
 
 (eval-after-load "web-mode"
   '(progn
@@ -294,7 +227,7 @@ might be bad.
 ;; Remove text in active region if inserting text
 (delete-selection-mode 1)
 
-(load-theme 'monokai t)
+(setq prelude-theme 'monokai)
 (scroll-bar-mode -1)
 (message "7. Config file has successfully loaded.")
 
@@ -355,23 +288,6 @@ With prefix P, create local abbrev. Otherwise it will be global."
   (interactive "p")
   (endless/forward-paragraph (- n)))
 
-
-;; http://endlessparentheses.com/emacs-narrow-or-widen-dwim.html
-(defun narrow-or-widen-dwim (p)
-  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
-Intelligently means: region, subtree, or defun, whichever applies
-first.
-
-With prefix P, don't widen, just narrow even if buffer is already
-narrowed."
-  (interactive "P")
-  (declare (interactive-only))
-  (cond ((and (buffer-narrowed-p) (not p)) (widen))
-        ((region-active-p)
-         (narrow-to-region (region-beginning) (region-end)))
-        ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
-        (t (narrow-to-defun))))
-
 ;; http://www.masteringemacs.org/articles/2010/12/22/fixing-mark-commands-transient-mark-mode/
 (defun push-mark-no-activate ()
   "Pushes `point' to `mark-ring' and does not activate the region
@@ -401,7 +317,7 @@ This is the same as using \\[set-mark-command] with the prefix argument."
   "Enter the interactive Python environment"
   (interactive)
   (progn
-    (insert "!import code; code.interact(local=vars())")
+    (insert "from IPython import embed; embed()")
     (move-end-of-line 1)
     (comint-send-input)))
 ;; (global-set-key (kbd "C-c i") 'python-interactive)
@@ -410,241 +326,6 @@ This is the same as using \\[set-mark-command] with the prefix argument."
 (define-key sp-keymap (kbd "M-k") 'sp-kill-hybrid-sexp)
 (define-key sp-keymap (kbd "C-]") 'sp-select-next-thing-exchange)
 (define-key sp-keymap (kbd "C-<left_bracket>") 'sp-select-previous-thing)
-
-
-(setq last-kbd-macro
-   "\C-u\C-u\353")
-
-
-(defun elpy-nav-forward-block ()
-  "Move to the next line indented like point.
-This will skip over lines and statements with different
-indentation levels."
-  (interactive)
-  (let ((indent (current-column))
-        (start (point)))
-    (when (/= (% indent python-indent-offset)
-              0)
-      (setq indent (* (1+ (/ indent python-indent-offset))
-                      python-indent-offset)))
-    (python-nav-forward-statement)
-    (while (and (< indent (current-indentation))
-                (not (eobp)))
-      (python-nav-forward-statement))
-    (when (< (current-indentation)
-             indent)
-      (goto-char start))))
-
-(defun elpy-nav-backward-block ()
-  "Move to the previous line indented like point.
-This will skip over lines and statements with different
-indentation levels."
-  (interactive)
-  (let ((indent (current-column))
-        (start (point)))
-    (when (/= (% indent python-indent-offset)
-              0)
-      (setq indent (* (1+ (/ indent python-indent-offset))
-                      python-indent-offset)))
-    (python-nav-backward-statement)
-    (while (and (< indent (current-indentation))
-                (not (bobp)))
-      (python-nav-backward-statement))
-    (when (< (current-indentation)
-             indent)
-      (goto-char start))))
-
-(defun elpy-nav-forward-indent ()
-  "Move forward to the next indent level, or over the next word."
-  (interactive)
-  (if (< (current-column) (current-indentation))
-      (let* ((current (current-column))
-             (next (* (1+ (/ current python-indent-offset))
-                      python-indent-offset)))
-        (goto-char (+ (point-at-bol)
-                      next)))
-    (let ((eol (point-at-eol)))
-      (forward-word)
-      (when (> (point) eol)
-        (goto-char (point-at-bol))))))
-
-(defun elpy-nav-backward-indent ()
-  "Move backward to the previous indent level, or over the previous word."
-  (interactive)
-  (if (and (<= (current-column) (current-indentation))
-           (/= (current-column) 0))
-      (let* ((current (current-column))
-             (next (* (1- (/ current python-indent-offset))
-                      python-indent-offset)))
-        (goto-char (+ (point-at-bol)
-                      next)))
-    (backward-word)))
-
-(defun elpy-nav--iblock (direction skip)
-  "Move point forward, skipping lines indented more than the current one.
-DIRECTION should be 1 or -1 for forward or backward.
-SKIP should be #'> to skip lines with larger indentation or #'<
-to skip lines with smaller indentation."
-  (let ((start-indentation (current-indentation)))
-    (python-nav-forward-statement direction)
-    (while (and (not (eobp))
-                (not (bobp))
-                (or (looking-at "^\\s-*$")
-                    (funcall skip
-                             (current-indentation)
-                             start-indentation)))
-      (python-nav-forward-statement direction))))
-
-(defun elpy-nav-move-iblock-down (&optional beg end)
-  "Move the current indentation block below the next one.
-With an active region, move that instead of the current block.
-An indentation block is a block indented further than the current
-one."
-  (interactive "r")
-  (let ((use-region (use-region-p))
-        (startm (make-marker))
-        (starti nil)
-        (midm (make-marker))
-        (midi nil)
-        (endm (make-marker))
-        (deactivate-mark nil))
-    (save-excursion
-      (when use-region
-        (goto-char beg))
-      (set-marker startm (line-beginning-position))
-      (setq starti (current-indentation))
-      (if use-region
-          (progn
-            (goto-char end)
-            (when (> (current-column)
-                     0)
-              (forward-line 1)))
-        (elpy-nav--iblock 1 #'>))
-      (set-marker midm (line-beginning-position))
-      (setq midi (current-indentation))
-      (elpy-nav--iblock 1 #'>)
-      (goto-char (line-beginning-position))
-      (when (<= (current-indentation)
-                starti)
-        (when (/= (skip-chars-backward "[:space:]\n") 0)
-          (forward-line 1)))
-      (when (and (= midm (point))
-                 (/= (point)
-                     (line-end-position))
-                 (= (line-end-position)
-                    (point-max)))
-        (goto-char (point-max))
-        (insert "\n"))
-      (set-marker endm (line-beginning-position)))
-    (when (and (/= startm midm)
-               (/= midm endm)
-               (/= startm endm)
-               (= starti midi))
-      (goto-char endm)
-      (insert (buffer-substring startm midm))
-      (when use-region
-        (set-mark (point)))
-      (delete-region startm midm)
-      (goto-char endm)
-      (back-to-indentation))))
-
-(defun elpy-nav-move-iblock-up (&optional beg end)
-  "Move the current indentation block below the next one.
-With an active region, move that instead of the current block.
-An indentation block is a block indented further than the current
-one."
-  (interactive "r")
-  (let ((use-region (use-region-p))
-        (startm (make-marker))
-        (starti nil)
-        (midm (make-marker))
-        (midi nil)
-        (endm (make-marker))
-        (deactivate-mark nil))
-    (save-excursion
-      (when use-region
-        (goto-char beg))
-      (set-marker startm (line-beginning-position))
-      (setq starti (current-indentation))
-      (if use-region
-          (progn
-            (goto-char end)
-            (when (> (current-column)
-                     0)
-              (forward-line 1)))
-        (elpy-nav--iblock 1 #'>)
-        (cond
-         ((and (save-excursion
-                 (goto-char (line-end-position))
-                 (and (> (current-column) 0)
-                      (= (point-max) (point)))))
-          (goto-char (line-end-position))
-          (insert "\n"))
-         ((< (current-indentation)
-             starti)
-          (when (/= (skip-chars-backward "[:space:]\n") 0)
-            (forward-line 1)))))
-      (set-marker midm (line-beginning-position))
-      (goto-char startm)
-      (elpy-nav--iblock -1 #'>)
-      (goto-char (line-beginning-position))
-      (set-marker endm (line-beginning-position))
-      (setq midi (current-indentation)))
-    (when (and (/= startm midm)
-               (/= midm endm)
-               (/= startm endm)
-               (= starti midi))
-      (goto-char endm)
-      (insert (buffer-substring startm midm))
-      (when use-region
-        (set-mark (point)))
-      (delete-region startm midm)
-      (goto-char endm)
-      (back-to-indentation))))
-
-(defun elpy-nav-move-iblock-left ()
-  "Dedent the current indentation block, or the active region."
-  (interactive)
-  (let (beg end)
-    (if (use-region-p)
-        (setq beg (region-beginning)
-              end (region-end))
-      (save-excursion
-        (setq beg (line-beginning-position))
-        (elpy-nav--iblock 1 #'>)
-        (setq end (line-beginning-position))))
-    (python-indent-shift-left beg end)))
-
-(defun elpy-nav-move-iblock-right ()
-  "Indent the current indentation block, or the active region."
-  (interactive)
-  (let (beg end)
-    (if (use-region-p)
-        (setq beg (region-beginning)
-              end (region-end))
-      (save-excursion
-        (setq beg (line-beginning-position))
-        (elpy-nav--iblock 1 #'>)
-        (setq end (line-beginning-position))))
-    (python-indent-shift-right beg end)))
-
-(global-set-key (kbd "<s-down>") 'elpy-nav-forward-block)
-(global-set-key (kbd "<s-up>") 'elpy-nav-backward-block)
-(global-set-key (kbd "<s-left>") 'elpy-nav-backward-indent)
-(global-set-key (kbd "<s-right>") 'elpy-nav-forward-indent)
-(global-set-key (kbd "<s-M-down>") 'elpy-nav-move-iblock-down)
-(global-set-key (kbd "<s-M-up>") 'elpy-nav-move-iblock-up)
-(global-set-key (kbd "<s-M-left>") 'elpy-nav-move-iblock-left)
-(global-set-key (kbd "<s-M-right>") 'elpy-nav-move-iblock-right)
-
-(global-aggressive-indent-mode 1)
-(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
-(add-to-list 'aggressive-indent-excluded-modes 'python-mode)
-
-(define-key sp-keymap (kbd "M-k") 'sp-kill-hybrid-sexp)
-(define-key sp-keymap (kbd "C-]") 'sp-select-next-thing-exchange)
-(define-key sp-keymap (kbd "C-<left_bracket>") 'sp-select-previous-thing)
-
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
@@ -659,3 +340,35 @@ one."
       highlight-symbol-on-navigation-p t)
 (global-set-key (kbd "M-n") 'highlight-symbol-next)
 (global-set-key (kbd "M-p") 'highlight-symbol-prev)
+
+(defun sacha/vsplit-last-buffer (prefix)
+  "Split the window vertically and display the previous buffer."
+  (interactive "p")
+  (split-window-vertically)
+  (other-window 1 nil)
+  (unless prefix
+    (switch-to-next-buffer)))
+(defun sacha/hsplit-last-buffer (prefix)
+  "Split the window horizontally and display the previous buffer."
+  (interactive "p")
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (unless prefix (switch-to-next-buffer)))
+(global-set-key (kbd "C-x 2") 'sacha/vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'sacha/hsplit-last-buffer)
+
+;; https://github.com/sachac/.emacs.d/blob/gh-pages/Sacha.org#pop-to-mark
+(global-set-key (kbd "C-x p") 'pop-to-mark-command)
+(setq set-mark-command-repeat-pop t)
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+(setq helm-buffers-fuzzy-matching t) ; fuzzy matching buffer names when non--nil
+
+(autoload 'helm-company "helm-company") ;; Not necessary if using ELPA package
+(eval-after-load 'company
+  '(progn
+     (define-key company-mode-map (kbd "C-:") 'helm-company)
+     (define-key company-active-map (kbd "C-:") 'helm-company)))
+
+(setq langtool-language-tool-jar "~/.emacs.d/LanguageTool-2.7/languagetool-commandline.jar")
